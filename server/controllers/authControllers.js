f75f4e1580e15629fcc215f7eaa65b5eea6826d0
Cauan <cauanfailla@gmail.com>
2025-08-17 13:19:26 -0300

    Criando api para buscar dados do usuário

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt'
 import {createUser, findUserByEmail} from '../models/user.js'
 
-export function cadastro(req, res) {
+export async function cadastro(req, res) {
     const {name, email, password} = req.body
-    const hash = bcrypt.hash(password, 10)
+    const hash = await bcrypt.hash(password, 10)
     createUser(name, email, hash)
     res.redirect('/')
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,5 @@ import pageRoutes from './pageRoutes.js'
 const router = Router()
 router.use('/', pageRoutes)
 router.use('/auth', authRoutes)
+router.use('/user', userRoutes)
 export default router
