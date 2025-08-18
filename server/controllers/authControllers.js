dd0a62b46cd7dbc29817fad5a11e0f49ca46ba5c
Cauan <cauanfailla@gmail.com>
2025-08-18 13:52:17 -0300

    Adicionando header na página do Dashboard

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,10 +1,14 @@
 import bcrypt from 'bcrypt'
-import {createUser, findUserByEmail} from '../models/user.js'
+import {createUser, findUserByEmail, findUserById} from '../models/user.js'
 
 export async function cadastro(req, res) {
     const {name, email, password} = req.body
     const hash = await bcrypt.hash(password, 10)
-    createUser(name, email, hash)
+    const userId = await createUser(name, email, hash)
+    const user = await findUserById(userId)
+    req.session.userId = user.id
+    req.session.userName = user.name
+    req.session.userEmail = user.email
     res.redirect('/')
 }
 
--- server/models/user.js
+++ server/models/user.js
@@ -11,3 +11,9 @@ export async function findUserByEmail(email) {
     const [result] = await pool.query(sql, [email])
     return result[0]
 }
+
+export async function findUserById(id) {
+    const sql = 'SELECT * FROM users WHERE id = ?'
+    const [result] = await pool.query(sql, [id])
+    return result[0]
+}
